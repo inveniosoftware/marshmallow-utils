fd2ba4fc4112cf0187f6c2578d8f7b7d8ba5c29e
Guillaume Viger <fenekku@fenekku.com>
2020-10-01 13:26:00 -0500

    links: parametrize when a Link is serialized

--- marshmallow_utils/fields/links.py
+++ marshmallow_utils/fields/links.py
@@ -18,13 +18,20 @@ class Links(fields.Field):
 
     def _serialize(self, value, attr, obj, *args, **kwargs):
         """Dump the field by using the contextual schema."""
-        factory = self.context.get("links_factory") or {}
-        namespace = self.context.get("links_namespace", {})
-        schema = factory.get_schema(namespace, self.context)
+        factory = self.context.get("links_factory")
+        namespace = self.context.get("links_namespace")
+        schema = (
+            factory.get_schema(namespace, self.context) if factory else None
+        )
         if schema:
             return schema.dump(obj)
         else:
             return {}
+
+
+def always(*args, **kwargs):
+    """A function that returns True no matter what is passed to it."""
+    return True
 
 
 class Link(fields.Field):
@@ -33,11 +40,13 @@ class Link(fields.Field):
     # NOTE: forces serialization
     _CHECK_ATTRIBUTE = False
 
-    def __init__(self, template=None, params=None, permission=None, **kwargs):
+    def __init__(self, template=None, params=None, permission=None,
+                 when=always, **kwargs):
         """Constructor."""
         self.template = template
         self.permission = permission
         self.params = params
+        self.when = when
         super().__init__(**kwargs)
 
     def _serialize(self, value, attr, obj, *args, **kwargs):
@@ -48,4 +57,8 @@ class Link(fields.Field):
         if field_permission_check and self.permission:
             if not field_permission_check(self.permission):
                 return missing
+
+        if not self.when(obj):
+            return missing
+
         return factory.create_link(self.template, self.params(obj))
