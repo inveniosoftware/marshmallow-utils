d1e834f35bed56c65819444d871ef7f4edc8a6bc
Nicola Tarocco <ntarocco@gmail.com>
2022-04-12 11:02:41 +0200

    doc: remove fail on warning
    * Sphinx fails on Python 3.8 with warnings related to wrong targets that
      are unclear. Disabled failing on warnings.

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,14 @@
 # Marshmallow-Utils is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-check-manifest --ignore ".*-requirements.txt" && \
-sphinx-build -qnNW docs docs/_build/html && \
-pytest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnN docs docs/_build/html
+python -m pytest
+tests_exit_code=$?
+exit "$tests_exit_code"
