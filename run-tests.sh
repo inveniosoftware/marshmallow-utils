6e5f43f04edba19dcd23f551fea0391d90c1aa4c
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-05-18 22:58:44 +0200

    global: clean test infrastructure

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020-2022 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Marshmallow-Utils is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -12,7 +13,7 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnN docs docs/_build/html
 python -m pytest
 tests_exit_code=$?
